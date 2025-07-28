266b566debe21024ee234ccb13c60ceb239cbc2b
Soojeong <soojlee0106@naver.com>
2025-07-28 18:44:31 +0900

    fix: setup mysql connection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use std::env;
+
 use axum::{Router, routing::get};
+use deadpool_diesel::{
+    Runtime,
+    mysql::{Manager, Pool},
+};
+use dotenvy::dotenv;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").unwrap();
+    let manager = Manager::new(db_url, Runtime::Tokio1);
+    let pool = Pool::builder(manager).build().unwrap();
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .with_state(pool);
+
+    let listener = TcpListener::bind("localhost:3000").await.unwrap();
+
     axum::serve(listener, app).await.unwrap();
 }
