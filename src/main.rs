1df280797252692a705e0bead6d8ce3e7d09f36d
Soojeong <soojlee0106@naver.com>
2025-07-28 19:24:08 +0900

    fix: add cors layer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::env;
 
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    http::{HeaderValue, Method, header::AUTHORIZATION, header::CONTENT_TYPE},
+    routing::get,
+};
 use deadpool_diesel::{
     Runtime,
     mysql::{Manager, Pool},
 };
 use dotenvy::dotenv;
 use tokio::net::TcpListener;
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
@@ -16,11 +21,18 @@ async fn main() {
     let manager = Manager::new(db_url, Runtime::Tokio1);
     let pool = Pool::builder(manager).build().unwrap();
 
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, CONTENT_TYPE]);
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
+        .layer(cors)
         .with_state(pool);
 
-    let listener = TcpListener::bind("localhost:3000").await.unwrap();
+    let listener = TcpListener::bind("localhost:3200").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
