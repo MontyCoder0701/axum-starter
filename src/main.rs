3db1533bdbf4a6dedd61c72b8bf5369a2b1842d9
Soojeong <soojlee0106@naver.com>
2025-07-29 10:48:33 +0900

    refactor: apply const values for config params

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,17 @@ mod user;
 async fn main() {
     dotenv().ok();
 
+    const PORT: u16 = 3200;
+    const HOST: &'static str = "localhost";
+
     let db_url = env::var("DATABASE_URL").unwrap();
+    let client_url = env::var("CLIENT_URL").unwrap();
+
     let manager = Manager::new(db_url, Runtime::Tokio1);
     let pool = Pool::builder(manager).build().unwrap();
 
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin(client_url.parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, CONTENT_TYPE]);
@@ -36,7 +41,7 @@ async fn main() {
         .layer(cors)
         .with_state(pool);
 
-    let listener = TcpListener::bind("localhost:3200").await.unwrap();
+    let listener = TcpListener::bind(format!("{HOST}:{PORT}")).await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
